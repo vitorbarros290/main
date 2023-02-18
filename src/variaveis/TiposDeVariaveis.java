19628c29308f60c2c7be0aa1a6fb921b84608ee1
vitorbarros01 <vitorbarros290@gmail.com>
2023-02-17 20:25:38 -0400

    enviando

---
+++ src/variaveis/TiposDeVariaveis.java
@@ -0,0 +1,33 @@
+package variaveis;
+
+public class TiposDeVariaveis {
+
+	public static void main(String[] args) {
+		//eu realmente não sei como isso funciona
+	/*variavel
+	 * inteiro
+	 * real
+	 * string
+	 * logic*/
+		int idade = 25;
+		//idade = 25
+			int livros;
+	        int cadernos;
+			int canetas;
+		
+			int alunos = 5;
+			livros = 10;
+			canetas = 5;
+			cadernos = 2 + 3;
+			int a = 2;
+			int b = 8;
+			int c = 9;
+		
+			int resultado = a + b * c;
+			
+			
+			
+ System.out.println("O valor do resultado" + resultado );
+	}
+
+}
